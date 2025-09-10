154c6b2df048fb14904564df65892fcc770faab3
yazidgo <yaziddaud9@gmail.com>
2025-09-10 14:47:02 +0700

    ini File Js

---
+++ script.js
@@ -0,0 +1,110 @@
+// ini tuganya js
+
+// ini variabel
+
+var no1 = 5;
+no1 = 3;
+console.log(no1); //3
+// window.alert(no1); //3
+
+console.log("=========ini penjumlahan==========");
+no1 = no1 + 11;
+console.log(no1); // ini hasilnya 14 karena 3 + 11 = 14
+
+console.log("=========ini pengurangan==========");
+no1 = no1 - 7;
+console.log(no1); // ini hasilnya 7 karena 14 - 7 = 7
+
+console.log("=========ini perkalian==========");
+var no2 = no1 * 10;
+console.log(no2); // ini hasilnya 70 karena 7 * 10 = 70
+
+console.log("=========ini pembagian==========");
+var no3 = no2 / no1;
+console.log(no3); // ini hasilnya 10 karena 70 / 7 = 10
+
+
+
+// ini type Variabel
+console.log("======== ini variabel String ===========");
+varString = "Hi I am Moch Yazid Al Bustomi";
+console.log(varString);
+
+console.log("======== Ini Varabel Integer===========");
+var myInteger = 10; // ini tipe data integer
+console.log(myInteger);
+
+console.log("========== ini Varabel Float=========");
+var myFloat = 10.5; // ini tipe data float
+console.log(myFloat);
+
+console.log("========== ini variabel Long atau panjang =========");
+var myLong = 12345678901234567890n; // ini tipe data long
+console.log(myLong);
+
+console.log("========= ini variabel boolean==========");
+var myBoolean = true; // ini tipe data boolean
+console.log(myBoolean);
+
+console.log("========== ini bolean 2=========");
+var myBoolean2 = false; // ini tipe data boolean
+console.log(myBoolean2);
+
+console.log("============ not =======");
+var myUndefined; // ini tipe data undefined
+console.log(myUndefined);
+
+console.log("======== ini NUll ===========");
+var myNull = null; // ini tipe data null
+console.log(myNull);
+
+console.log("========== ini Double =========");
+var myDouble =931014141982.22; // ini tipe data double
+console.log(myDouble);
+
+console.log("========== ini NaN=========");
+var NaN_Example = 0 / 0; // ini tipe data NaN (Not a Number)
+console.log(NaN_Example);
+
+console.log("===================");
+console.log("Hello World");
+console.log("===================");
+
+// ini Array dan Object
+console.log("===================");
+var myArray = []; // empty array
+console.log(myArray);
+
+console.log("===================");
+var favoriteName = [ "Moch" , "Yazid" , "Al Bustomi" ];
+console.log(favoriteName);
+
+console.log("===================");
+var carsInParkingLot = [ "Meclaren" , "Ferrari" , "Bugati" ];
+console.log(carsInParkingLot);
+
+console.log("===================");
+var employees = [ "Yased", "Ipan" , "Dimas" ];
+console.log(employees);
+
+console.log("===================");
+var primeNumbers = [ 2 , 3 , 5 , 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 ];
+console.log(primeNumbers);
+
+console.log("===================");
+var randomVariables = [ 2 , "any type works" , undefined , null , true , 2.51 ];
+myArray = [ "zero" , "one" , "two" ];
+console.log(randomVariables);
+
+window.alert ( myArray[ 0 ]); // 0
+elementNumber = 1 ;
+window.alert ( myArray[ elementNumber]); // Billy
+myObject = {};
+
+john = { firstname: "John" , lastname: "Doe" , fullname: "John Doe" };
+billy = {firstname : "Billy" ,lastname: undefined ,fullname: "Billy"};
+window.alert ( john.fullname ); // John Doe
+window.alert ( billy.firstname ); // Billy
+
+
+
